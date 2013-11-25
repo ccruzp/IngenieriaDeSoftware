ae3fcc9d6ea172fcb42505b5dd0ea7b78e3cf569
Luis Miranda <lcmm1993@gmail.com>
2013-11-25 16:35:36 -0430

    se agregaron comentarios

--- Proyecto/TestUnit.py
+++ Proyecto/TestUnit.py
@@ -94,23 +94,26 @@ class PruebaArticulo(unittest.TestCase):
 
         assert str(self.Articulo) == self.Articulo.tema+' '+self.Articulo.titulo+': '+str(self.Articulo.calificacion)
 
-
+# Modulo de prueba de la clase ComitePrograma
 class PruebaComitePrograma(unittest.TestCase):
 
     def setUp(self):
         self.comite = ComitePrograma()
         
+    # prueba del metodo addMiembro
     def testAddMiembro(self):
         miembro1 = Miembro("Jose", "USB", "jusb@hotmail.com" ,"1342", "1234567")
         miembro2 = Miembro("Pedro", "BSU", "pusb@hotmail.com" ,"1432", "1243567")
 
         self.comite.addMiembro(miembro1)
-        assert self.comite.miembros == [miembro1], "No esta agregando a miembro 1"
+        assert self.comite.miembros == [miembro1]\
+            , "No esta agregando a miembro 1"
 
         self.comite.addMiembro(miembro2)
         assert self.comite.miembros == [miembro1,miembro2],\
             "No esta agregando a miembro 2"
 
+    # prueba del metodo setPresi
     def testSetPresi(self):
         presi = Miembro("Manic","BUS","mic@mand.te","8888","88888")
         miembro1 = Miembro("Jose", "USB", "jusb@hotmail.com" ,"1342", "1234567")
@@ -119,29 +122,36 @@ class PruebaComitePrograma(unittest.TestCase):
         self.comite.addMiembro(miembro1)
         self.comite.addMiembro(miembro2)
 
+        # prueba sobre colocar a un no miembro como presidente
         assert not self.comite.setPresi(presi), "Coloco como presi un no miembro"
 
+        # prueba sobre colocar a un recien agregado como presidente
         self.comite.addMiembro(presi)
         assert self.comite.setPresi(presi) and self.comite.presi == presi,\
             "Fallo colocando como presi a alguien que acabo de agregar"
 
+        # prueba sobre colocar a cualquier miembro como presidente
         for x in self.comite.miembros:
             assert self.comite.setPresi(x) and self.comite.presi == x,\
                 "Falla colocando como presi a un miembro cualquiera"
 
+# Modulo de prueba de la clase CLEI
 class pruebaCLEI(unittest.TestCase):
 
     def setUp(self):
         self.clei = CLEI()
 
+    # prueba del metodo addComites
     def testAddComites(self):
         
+        # pruebas agregando 100 comites
         for i in range(0,100):
             self.clei.addComites("topico"+str(i))
-            
+        
             assert len(self.clei.comites) == self.clei.numComi,\
                 "No se estan agregando comite "+str(i)
 
+    # prueba del metodo setPresidente
     def testSetPresidente(self):
 
         self.clei.addComites("topico")
@@ -151,7 +161,7 @@ class pruebaCLEI(unittest.TestCase):
         assert self.clei.setPresidente('1',self.clei.comites['1'].miembros[0]),\
             "No esta agregando presidente"
 
-        
+    # prueba del metodo addMiembro
     def testAddMiembro(self):
 
         self.clei.addComites("topico")
@@ -174,7 +184,7 @@ class pruebaCLEI(unittest.TestCase):
         assert str(comitePrueba) == str(self.clei.comites['1']),\
             "No se estan creando bien los comites"
 
-        
+    # prueba del metodo nuevoArticulo
     def testNuevoArticulo(self):
         self.clei.nuevoArticulo("Titulo", "Tema")
         assert len(self.clei.articulos) == 1, "Falló creación"
@@ -182,6 +192,7 @@ class pruebaCLEI(unittest.TestCase):
         assert self.clei.articulos[0].tema == "Tema", "El articulo se creó mal"
         self.clei.articulos[:] = []
 
+    # prueba del metodo llenarListas
     def testLlenarListas(self):
         self.clei.nuevoArticulo("Uno", "Prueba")
         self.clei.articulos[0].calificar(2)
